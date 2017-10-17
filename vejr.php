92990c4a0c42f0ca9622cdc6f57d09e79da806d6
Simon Rasmussen <simon@huebanden.dk>
2017-10-17 10:22:49 +0200

    added vejr

---
+++ vejr.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Simon_
+ * Date: 17-10-2017
+ * Time: 09:23
+ */
+
+function getVejrJson()
+{
+
+
+    // Start cURL
+    $ch = curl_init();
+
+    // Set cURL options
+//        curl_setopt($ch, CURLOPT_URL, 'http://vejr.eu/api.php?location=' . $location . '&degree=C');
+    curl_setopt($ch, CURLOPT_URL, 'http://vejr.eu/api.php?location=Roskilde&degree=C');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'apivejr');
+
+    // Parse result
+    $result = curl_exec($ch);
+
+    // Close connection when done
+    curl_close($ch);
+
+    // Return our decoded result
+    return json_decode($result);
+
+
+}
+
+// Test CVRAPI
+print_r(getVejrJson('Roskilde'));
+print_r(getVejrJson());
+?>
